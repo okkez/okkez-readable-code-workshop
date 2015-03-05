0b7d72800321f5e5095d93add0d8137987ac97e3
okkez <okkez000@gmail.com>
2015-03-05 22:39:25 +0900

    Read a recipe from file

--- src/net/okkez/recipe/Recipe.java
+++ src/net/okkez/recipe/Recipe.java
@@ -1,8 +1,22 @@
 package net.okkez.recipe;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Recipe {
 
 	public static void main(String[] args) {
-		System.out.println("オムライス");
+		File file = new File("recipe-data.txt");
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String recipe = reader.readLine();
+			reader.close();
+			System.out.println(recipe);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
